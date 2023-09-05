d4fc5cd7491c75c36e26ee0a0fabdef1a94822b3
DandieKYT <75677738+DandieKYT@users.noreply.github.com>
2023-09-05 23:23:28 +0300

    full ui

--- src/test/java/tests/api/CheckCodeTest.java
+++ src/test/java/tests/api/CheckCodeTest.java
@@ -2,23 +2,23 @@ package tests.api;
 
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
 import static io.restassured.http.ContentType.JSON;
-import static org.hamcrest.Matchers.is;
+import static specs.Specification.requestSpec;
+import static specs.Specification.responseSpec;
+
 
 public class CheckCodeTest {
-@Test
-void status() {
-    given()
-            .log().uri()
-            .contentType(JSON)
-            .when()
-            .get("https://javarush.com/api/1.0/rest/me")
-            .then()
-            .log().status()
-            .log().body()
-            .statusCode(200);
-//            .body("message", is("test"));
+    @Test
+    void status() {
+        String message = "{message: \"124\", parentCommentId: null, replyToUserId: null}";
+        given(requestSpec)
+                .body(message)
+                .when()
+                .post("/rest/discussions/discussion2332051/comments")
+                .then()
+                .spec(responseSpec)
+                .statusCode(201);
+    }
 
-}
 }
