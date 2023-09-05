522b35456a286b84fb5945d00f8a68a9173e3954
DandieKYT <75677738+DandieKYT@users.noreply.github.com>
2023-09-05 22:44:05 +0300

    full ui

--- src/test/java/tests/api/CheckCodeTest.java
+++ src/test/java/tests/api/CheckCodeTest.java
@@ -1,7 +1,24 @@
 package tests.api;
 
+import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
 
 public class CheckCodeTest {
+@Test
+void unSuccessfulLoginWithMissingEmailTest() {
+    given()
+            .log().uri()
+            .contentType(JSON)
+            .when()
+            .get("https://javarush.com/api/1.0/rest/me")
+            .then()
+            .log().status()
+            .log().body()
+            .statusCode(200);
+//            .body("message", is("test"));
 
+}
 }
