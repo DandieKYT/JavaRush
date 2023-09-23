a3b53a1a3d57df6698635c3bb6e15b11b3a4c0ce
DandieKYT <75677738+DandieKYT@users.noreply.github.com>
2023-09-23 22:12:28 +0300

    mobile

--- src/test/java/tests/mobile/CourseTask.java
+++ src/test/java/tests/mobile/CourseTask.java
@@ -1,14 +1,21 @@
 package tests.mobile;
 
 import io.appium.java_client.AppiumBy;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
-public class Suka extends TestBaseMobileRemote {
+public class CourseTask extends TestBaseMobileRemote {
     @Test
-    public void blyad(){
+    @CsvSource(value = {
+            "Course ,      Java course",
+            "Tasks,             You don't have any new tasks yet",
+    })
+    @DisplayName("Открытие вкладок Course/Java и проверка их содержимого")
+    public void checkCourseTask(){
         auth.loginBySecretKey();
         $(AppiumBy.id("com.hitechrush.jaxarush:id/bannerCloseButton")).click();
 
