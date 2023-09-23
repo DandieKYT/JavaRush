e4f65f9fb86cb3dfce24112f300106761248adea
DandieKYT <75677738+DandieKYT@users.noreply.github.com>
2023-09-23 22:47:25 +0300

    mobile

--- src/test/java/tests/mobile/CourseTask.java
+++ src/test/java/tests/mobile/CourseTask.java
@@ -1,21 +1,25 @@
 package tests.mobile;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class CourseTask extends TestBaseMobileRemote {
-    @Test
     @CsvSource(value = {
-            "Course ,      Java course",
-            "Tasks,             You don't have any new tasks yet",
+            "course,      Java course",
+            "tasks,             Tasks",
     })
-    @DisplayName("Открытие вкладок Course/Java и проверка их содержимого")
-    public void checkCourseTask(){
+    @ParameterizedTest
+    @DisplayName("Открытие разделов Course/Java и проверка их содержимого")
+    public void checkCourseTask(String param, String expectedText){
         auth.loginBySecretKey();
         $(AppiumBy.id("com.hitechrush.jaxarush:id/bannerCloseButton")).click();
 
@@ -29,7 +33,11 @@ public class CourseTask extends TestBaseMobileRemote {
         $(AppiumBy.id("com.hitechrush.jaxarush:id/questContainer")).click();
 
         $$(AppiumBy.className("android.widget.ImageButton")).get(0).click();
-        $$(AppiumBy.className("android.widget.ImageButton")).get(0).click();
+        $$(AppiumBy.className("android.widget.ImageButton")).get(0).click();//Нажимаеи назад
+
+        $$(AppiumBy.className("android.widget.ImageButton")).get(0).click();//Нажимаем на разделы
+        $$(AppiumBy.id(String.format("com.hitechrush.jaxarush:id/%s", param))).get(2).click();
+        $(AppiumBy.id("com.hitechrush.jaxarush:id/toolbarTitle")).shouldBe(Condition.text(expectedText));
 
     }
 }
