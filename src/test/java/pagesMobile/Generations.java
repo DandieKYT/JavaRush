4bd4605120096e1643e7324889a603d3debc49d2
DandieKYT <75677738+DandieKYT@users.noreply.github.com>
2023-09-26 21:55:11 +0300

    mobile

--- src/test/java/pagesMobile/Generations.java
+++ src/test/java/pagesMobile/Generations.java
@@ -3,7 +3,8 @@ package pagesMobile;
 import com.github.javafaker.Faker;
 
 public class Generations {
-    public static String generationRandomName() {
+    public static
+    String generationRandomName() {
         Faker faker = new Faker();
         return faker.name().firstName();
     }
--- src/test/java/tests/mobile/CommentTest.java
+++ src/test/java/tests/mobile/CommentTest.java
@@ -1,20 +1,28 @@
 package tests.mobile;
 
+import com.codeborne.selenide.Condition;
 import io.appium.java_client.AppiumBy;
+import org.checkerframework.checker.units.qual.A;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
-
+import static com.codeborne.selenide.Selenide.*;
+import static pagesMobile.Generations.generationRandomName;
+@Tag("Mobile")
 public class CommentTest extends TestBaseMobileRemote {
 @Test
     public void commTest(){
-    $(AppiumBy.id("com.hitechrush.jaxarush:id/commentsButton")).click(); //нажимем на кнопку коментарии
+    String name = generationRandomName();
+    stepsForApp();
+    $(AppiumBy.id("com.hitechrush.jaxarush:id/commentsButton")).click();//нажимем на кнопку коментарии
+    sleep(3000);
     $(AppiumBy.id("com.hitechrush.jaxarush:id/addCommentFab")).click(); // нажимаем добавить комент
     $(AppiumBy.id("com.hitechrush.jaxarush:id/addCommentText")).click();
-    $(AppiumBy.id("com.hitechrush.jaxarush:id/addCommentText")).sendKeys(gen.toString());
+    $(AppiumBy.id("com.hitechrush.jaxarush:id/addCommentText")).sendKeys("12314");
     $(AppiumBy.id("com.hitechrush.jaxarush:id/send")).click();
-    $$(AppiumBy.className("android.view.View"))
+    $$(AppiumBy.className("android.view.View")).get(0).equals("1112314");
+
+
 }
     private void stepsForApp() {
         auth.signInLink()
