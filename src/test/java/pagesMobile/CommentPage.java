f370b080c9f4ae85f11ac7b6545792b31be1fc3d
DandieKYT <75677738+DandieKYT@users.noreply.github.com>
2023-09-29 21:36:31 +0300

    mobile

--- src/test/java/pagesMobile/CommentPage.java
+++ src/test/java/pagesMobile/CommentPage.java
@@ -1,11 +1,14 @@
 package pagesMobile;
 
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import io.appium.java_client.AppiumBy;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class CommentPage {
-    public ElementsCollection
-            checkoutText = $$(AppiumBy.className("android.view.View"));
+    public  SelenideElement
+            checkoutText = $(AppiumBy.id("com.hitechrush.jaxarush:id/addCommentText"));
 }
--- src/test/java/tests/mobile/CommentTest.java
+++ src/test/java/tests/mobile/CommentTest.java
@@ -1,5 +1,6 @@
 package tests.mobile;
 
+import com.codeborne.selenide.Condition;
 import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.sleep;
 import static pagesMobile.Generations.generationRandomName;
 @Tag("Mobile")
-public class CommentTest extends TestBaseMobileRemote {
+public class CommentTest extends TestBaseMobile {
     String name, text;
 @Test
     public void commTest(){
@@ -21,8 +22,13 @@ public class CommentTest extends TestBaseMobileRemote {
     $(AppiumBy.id("com.hitechrush.jaxarush:id/addCommentText")).click();
     $(AppiumBy.id("com.hitechrush.jaxarush:id/addCommentText")).sendKeys(name);
     $(AppiumBy.id("com.hitechrush.jaxarush:id/send")).click();
-    text = comment.checkoutText.get(0).getText();
+    $(AppiumBy.xpath("(//android.widget.ImageView[@content-desc=\"JavaRush\"])[2]")).click();
+    $(AppiumBy.id("com.hitechrush.jaxarush:id/editButton")).click();
+    text = comment.checkoutText.getText();
     Assert.assertEquals(text, name);
+    $(AppiumBy.id("com.hitechrush.jaxarush:id/send")).click();
+
+    $(AppiumBy.id("com.hitechrush.jaxarush:id/userDisplayName")).shouldBe(Condition.text("KYT"));
     $(AppiumBy.xpath("(//android.widget.ImageView[@content-desc=\"JavaRush\"])[2]")).click();
     $(AppiumBy.id("com.hitechrush.jaxarush:id/deleteButton")).click();
 
