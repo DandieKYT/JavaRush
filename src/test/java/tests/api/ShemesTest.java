cf89b1aab054ad687327ad0aaa49a86e19a706a5
DandieKYT <75677738+DandieKYT@users.noreply.github.com>
2023-10-05 21:25:00 +0300

    API

--- src/test/java/tests/api/ShemesTest.java
+++ src/test/java/tests/api/ShemesTest.java
@@ -1,30 +1,33 @@
 package tests.api;
 
+import io.restassured.module.jsv.JsonSchemaValidator;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static specs.Specification.discussionsSpec;
 import static specs.Specification.intershipSpec;
 
 public class ShemesTest {
     @Test
-    public void intershipTest(){
+    public void intershipTest() {
         given()
                 .spec(intershipSpec)
                 .when()
                 .get("/articlecom.javarush.article.about.internship")
                 .then()
-                .body(matchesJsonSchemaInClasspath("schemes/intership.json"));
+                .body(matchesJsonSchemaInClasspath("schemes/article.json"));
     }
+
     @Test
-    public void project1Test(){
+    public void project1Test() {
         given()
                 .spec(discussionsSpec)
                 .when()
                 .get("/project1")
                 .then()
-                .body(matchesJsonSchemaInClasspath("schemes/project1.json"));
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemes/project1.json"));
     }
 
 }
