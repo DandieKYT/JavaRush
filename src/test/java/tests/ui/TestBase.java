924bc206700472d4001fa6434b71d236ebc91b19
DandieKYT <75677738+DandieKYT@users.noreply.github.com>
2023-10-13 20:03:17 +0300

    API

--- src/test/java/tests/ui/TestBase.java
+++ src/test/java/tests/ui/TestBase.java
@@ -13,6 +13,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import ui.steps.*;
