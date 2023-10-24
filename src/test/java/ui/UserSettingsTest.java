8871781b2308a60a1c3397d4152f47351db0fcba
DandieKYT <75677738+DandieKYT@users.noreply.github.com>
2023-10-24 23:05:01 +0300

    new code UI

--- src/test/java/ui/UserSettingsTest.java
+++ src/test/java/ui/UserSettingsTest.java
@@ -1,6 +1,9 @@
 package ui;
 
 import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +11,9 @@ import static com.codeborne.selenide.Selenide.*;
 import static ui.Common.stepsForWebApp;
 
 @Tag("UITest")
+@Owner("Кудрявцев Даниил")
+@Feature("Автотесты для UI")
+@Story("Настройки пользователя")
 public class UserSettingsTest extends TestBase {
 
     @Test
