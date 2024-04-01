b1c7b71f69f4e1d1b49b16f7c874d58907a31679
DandieKYT <kydrya911@gmail.com>
2024-04-01 14:16:22 +0300

    мпмо

--- src/main/java/ui/config/WebConfig.java
+++ src/main/java/ui/config/WebConfig.java
@@ -8,13 +8,16 @@ import org.aeonbits.owner.Config;
 public interface WebConfig extends Config {
 
     @Key("browser")
+    @DefaultValue("chrome")
     String getBrowser();
+
     @Key("browserVersion")
+    @DefaultValue("100.0")
     String getBrowserVersion();
 
     @Key("browserSize")
+    @DefaultValue("2500x1080")
     String getBrowserSize();
-
 
     @Key("videoUrl")
     String videoUrl();
