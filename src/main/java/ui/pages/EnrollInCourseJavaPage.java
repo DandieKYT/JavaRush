21c5b8afced3a8500394b38329c4bf6123b6eb85
DandieKYT <75677738+DandieKYT@users.noreply.github.com>
2024-04-20 15:00:18 +0300

    Final UI CODE

--- src/main/java/ui/pages/EnrollInCourseJavaPage.java
+++ src/main/java/ui/pages/EnrollInCourseJavaPage.java
@@ -6,6 +6,6 @@ import static com.codeborne.selenide.Selenide.$x;
 
 public class EnrollInCourseJavaPage {
 
-    public final SelenideElement clickButtonEnrollToCourse = $x("//*[contains(@class, 'hero-section')]//button[text()='Записаться на курс']");
+    public final SelenideElement clickButtonEnrollToCourse = $x("//*[contains(@class, 'btn--dark')]//span[text()='Записаться на курс']");
     public final SelenideElement checkoutForm = $x("//div[text()='Записаться на курс']");
 }
